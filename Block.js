fc884a8db2f8fd38c9d249695962ffce078e7c60
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-01-16 16:04:22 +0300

    Update Block.js

--- Block.js
+++ Block.js
@@ -3,31 +3,18 @@ class Block{
       var options = {
           restitution :0.4,
           friction :0.0,
+          visibilty : 255
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
-      this.image = loadImage("block.png")
-      this.visiblity = 255
     }
     display(){
-      image(this.image,200,100)
+
+
       var pos= this.body.position;
       rectMode(CENTER);
       rect(pos.x,pos.y,this.width, this.height);
-      
-      if(this.body.speed <3){
-        image(this.image,this.body.position.x,this.body.position.y,50,50) 
-      }
-      else{
-        World.remove(world,this.body)
-        push()
-        this.visiblity = this.visiblity-5
-        tint(255,this.visiblity)
-        image(this.image,this.body.position.x,this.body.position.y,50,50) 
-        pop()
-      }
-        }
-
-      }
+    }
+}
