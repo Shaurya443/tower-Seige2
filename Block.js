2314759f786ed1b5e583e6242c5184d10ee03ebe
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-01-16 16:13:53 +0300

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,20 +1,34 @@
 class Block{
-  constructor(x, y, width, height) {
-      var options = {
-          restitution :0.4,
-          friction :0.0,
-          visibilty : 255
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.visibility = 225
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
-    }
-    display(){
-
-
-      var pos= this.body.position;
-      rectMode(CENTER);
-      rect(pos.x,pos.y,this.width, this.height);
-    }
+      display(){
+        if(this.body.speed < 3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();}
+        else{
+          World.remove(world,this.body)
+          push()
+          this.visibility=this.visibility-5 
+          pop()
+        }
+      }
 }
