0d2cb1596690f25bc91db171c5872299213739e6
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-01-16 16:33:16 +0300

    Update Block.js

--- Block.js
+++ Block.js
@@ -28,7 +28,7 @@ class Block{
           World.remove(world,this.body)
           push()
           this.visibility=this.visibility-5 
-            tint(125,126);
+            tint(255,this.visibility);
           pop()
         }
       }
